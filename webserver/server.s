007b076118a6ee28faf8ba95afa66b269e9df4c0
Jonas Bird <jonas.bird.m@gmail.com>
2024-04-06 18:20:10 -0400

    feat: webServer: now binds to address

--- webserver/server.s
+++ webserver/server.s
@@ -6,10 +6,10 @@
 # version        : 0.01
 # created        : 2024/03/31
 # target         : x64
-
 .intel_syntax noprefix
 .global _start
 
+# stuff to remember rdi rsi rdx r10 r8 r9
 
 .section .text
 _start:
@@ -20,15 +20,27 @@ _start:
         xor rdx, rdx
 
 createSocket:
-        mov rdi, 2      # PF_INET
-        mov rsi, 1      # SOCK_STREAM type
+        mov rdi, 2        # PF_INET
+        mov rsi, 1        # SOCK_STREAM type
         mov rdx, 0        # IP
-        mov rax, 41       #SYS_SOCKETCALL
+        mov rax, 41       # SYS_SOCKETCALL
         syscall
-        cmp rax, 0
-        jne error
+
+        mov r12, rax      # socket fd is now in r12
+                          # r12 is safe according to SysV ABI
 
 bindAddress:
+# int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+        mov rdi, r12
+        lea rsi, [address] #put address here
+        mov rdx, 16
+        mov rax, 49       # bind syscall is 49
+        syscall
+        cmp rax, 0
+        jne error       # eventually I can add in specific error logging
+
+listen:
+
 
 exit:
         mov rdi, 0      # return code
@@ -39,6 +51,8 @@ error:
         jmp exit
 
 .section .data
-
-#tcpport:
-#       .port 8082
+address:
+        .2byte 2     # PF_INET/AF_INET
+        .2byte 0x921f  # 8082 for assignment needs to be 80 so 0x5000
+        .4byte 0     # run on all ports (0.0.0.0)
+        .8byte   0       # padding
