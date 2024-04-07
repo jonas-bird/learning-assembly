ada01de4c26547eb7d49756420331d9d7c7198de
Jonas Bird <jonas.bird.m@gmail.com>
2024-04-06 23:56:15 -0400

    feat: webserver: responds with 200 OK...
    still not a actual webserver, in fact it may as well be a Teapot

--- webserver/server.s
+++ webserver/server.s
@@ -32,14 +32,56 @@ createSocket:
 bindAddress:
 # int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
         mov rdi, r12
-        lea rsi, [address] #put address here
+        lea rsi, [sockaddr] #put address here
         mov rdx, 16
         mov rax, 49       # bind syscall is 49
         syscall
         cmp rax, 0
         jne error       # eventually I can add in specific error logging
 
+# int listen(int sockfd, int backlog);
 listen:
+        mov rdi, r12    # should still be there from bind, but just in case move socketfd into arg1
+        mov rax, 0x32     # listen syscall is 0x32
+        mov rsi, 0
+        syscall
+        cmp rax, 0
+        jne error
+
+
+loop1:
+
+#  int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+accept:
+        mov rax, 0x2b           # syscall for accept is 0x2b
+        mov rdi, r12            # move socketfd into arg1
+        mov rsi, 0       # arg2 is a NULL pointer for sockaddr of peer socket
+        mov rdx, 0              # NULL for length od peer socket
+        syscall
+        mov r13, rax    # move new socketfd into a safe spot
+
+# ssize_t read(int fd, void *buf, size_t count);
+readRequest:
+        mov rax, 0x0    # syscall for read
+        mov rdi, r13    # use the fd returned by accept
+        mov rdx, 255    # probably there is a better way to do this?
+        lea rsi, [requestBuffer]
+        syscall
+
+
+# ssize_t write(int fd, const void *buf, size_t count);
+writeResponse:
+       mov rdi, r13         # get the fd from accept
+       lea rsi, [response]  # pointer to our 'http response string'
+        mov rdx, 19         # the size of our response
+        mov rax, 0x01           # write syscall
+        syscall
+
+# int close(int fd)
+close:
+        mov rax, 3
+        mov rdi, r13
+        syscall
 
 
 exit:
@@ -48,11 +90,20 @@ exit:
         syscall
 
 error:
-        jmp exit
+        mov rdi, 1       # return code for something went wrong
+        mov rax, 60     # syscall exit
+        syscall
+
 
 .section .data
-address:
+sockaddr:
         .2byte 2     # PF_INET/AF_INET
         .2byte 0x921f  # 8082 for assignment needs to be 80 so 0x5000
-        .4byte 0     # run on all ports (0.0.0.0)
+        .4byte 0x0100007f     # 127.0.0.1 or run on all ports (0.0.0.0)
         .8byte   0       # padding
+
+response:
+        .string "HTTP/1.0 200 OK\r\n\r\n"  # should this be NULL terminated?
+
+.section .bss
+.lcomm requestBuffer, 256
