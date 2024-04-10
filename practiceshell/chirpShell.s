d16d2dc69221f82ce07b32b54e3e50bf0c4c79ef
Jonas Bird <jonas.bird.m@gmail.com>
2024-04-10 19:24:21 -0400

    feat: chirpshell added fork

--- practiceshell/chirpShell.s
+++ practiceshell/chirpShell.s
@@ -9,6 +9,14 @@ _start:
     nop
 loop:
     nop
+
+    mov rax, 10000000
+l0:
+    dec rax
+    cmp rax, 0
+    jne l0
+
+
     # print prompt write(1, &prompt, 2)
     mov rax, 1
     mov rdi, 1
@@ -24,19 +32,32 @@ loop:
     mov rdx, 1000
     syscall
 
+# replace newline with NULL byte
+l1:
+    mov al, [rsi]
+    cmp al, '\n'
+    je end1
+    inc rsi
+    jmp l1
+end1:
+    mov byte ptr [rsi], 0
 
-   mov BYTE PTR [input_buffer + rax - 1], 0
 
-    # write(1, &command, 50)
+    # pid_t fork(void);
+    mov rax, 57
+    syscall
+
+    cmp rax, 0
+    jne loop
+
     # exec(59)
-
+    # int execve(const char *pathname, char *const argv[], char *const envp[]);
     mov rax, 59
     mov rdi, offset command
     mov rsi, 0
     mov rdx, 0
     syscall
-    # return to start of event loop
-    jmp loop
+
 exit:
     // Exit 0
     mov rdi, 0      # return code
@@ -47,9 +68,6 @@ exit:
 .section .data
 prompt:
     .string "> "
-jonas:
-    .string "jonas\n"
-
 command:
     .ascii "/bin/"
 input_buffer:
